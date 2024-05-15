e96728f263e7a9566215436383cb94c6c58c0890
Phung Khac Nguyen <nguyenpk@yes4all.internal>
2024-05-15 15:13:40 +0700

    init source

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
+    "postcss-import": {},
+    "tailwindcss/nesting": "postcss-nesting",
     tailwindcss: {},
+    autoprefixer: {},
+    "postcss-preset-env": {
+      features: { "nesting-rules": false },
+    },
   },
 };
 
